549a68c0f96424de96c4d0bddf6f70676760521e
usigna <sylwia.fait@gmail.com>
2023-06-12 22:56:25 +0200

    Add new main page

--- js/app.js
+++ js/app.js
@@ -19,8 +19,31 @@ function showHamburgerMenu() {
   hamburger.addEventListener('click', handleClick);
 }
 
+function blobAnimate() {
+  const blobBtn = document.querySelector('.blob-btn');
+  const blobBtnImage = blobBtn.querySelector('img');
+  const blobImage = document.querySelector('.blob-img');
+
+  blobBtn.addEventListener('click', function() {
+    if (blobBtnImage.classList.contains('pause')) {
+      blobImage.src = 'img/blob.svg';
+      blobBtnImage.src = 'img/play-solid.svg';
+      blobBtnImage.alt = 'Wznów animację.';
+      blobBtnImage.classList.add('padding-left');
+      blobBtnImage.classList.remove('pause');
+    } else {
+      blobImage.src = 'img/blob-animate.svg';
+      blobBtnImage.src = 'img/pause-solid.svg';
+      blobBtnImage.alt = 'Zatrzymaj animację.';
+      blobBtnImage.classList.remove('padding-left');
+      blobBtnImage.classList.add('pause');
+    }
+  })
+};
+
 const init = function () {
   showHamburgerMenu();
+  blobAnimate();
 };
 
 document.addEventListener('DOMContentLoaded', init);
