3c61c4e4ddfbf73c78d16674b86983b349b756a6
usigna <sylwia.fait@gmail.com>
2024-03-08 11:37:41 +0100

    Add new

--- js/app.js
+++ js/app.js
@@ -1,48 +1,93 @@
-function stopAnimations() {
-  const btn = document.querySelector('.accessibility-options__btn');
-  const typingText = document.querySelector('.typing-text');
-  const blobImage = document.querySelector('.blob-img');
-  // const offerImage = document.querySelector('.offer__item--animation img');
-  const duckyImage = document.querySelector('.ducky-contact');
-
-  btn.addEventListener('click', function() {
-
-    if (btn.classList.contains('play')) {
-      typingText.classList.remove('typing-text--animate');
-      blobImage.src = 'img/blob.svg';
-      // offerImage.style.animation = '0';
-      duckyImage.style.animation = '0';
-      btn.classList.remove('play');
-    } else {
-      typingText.classList.add('typing-text--animate');
-      blobImage.src = 'img/blob-animate.svg';
-      // offerImage.style.animation = 'swinging 2s linear infinite';
-      duckyImage.style.animation = 'swinging-small 2.5s linear infinite';
-      btn.classList.add('play');
-    }
-  })
+function handleFirstTab(e) {
+  if (e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing');
+    window.removeEventListener('keydown', handleFirstTab);
+  }
 };
 
-function changeSlide() {
-  const slidesContainer = document.querySelector(".posts");
-  const slide = document.querySelector(".posts__item");
-  const prevButton = document.querySelector(".slider__btn--left");
-  const nextButton = document.querySelector(".slider__btn--right");
+window.addEventListener('keydown', handleFirstTab);
 
-  nextButton.addEventListener("click", function() {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft += slideWidth;
+function useCustomCursor() {
+  const cursor = document.querySelector('.cursor');
+  const cursorScale = document.querySelectorAll('.cursor-scale');
+  let mouseX = 0;
+  let mouseY = 0;
+
+  gsap.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function() {
+      gsap.set(cursor, {
+        css: {
+          left: mouseX,
+          top: mouseY
+        }
+      })
+    }
+  });
+  
+  window.addEventListener('mousemove', function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
   });
 
-  prevButton.addEventListener("click", function() {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft -= slideWidth;
+  cursorScale.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+      cursor.classList.remove('grow');
+      cursor.classList.remove('grow-small');
+    });
+
+    link.addEventListener('mousemove', () => {
+      cursor.classList.add('grow');
+      cursor.classList.remove('grow-small');
+
+      if(link.classList.contains('small')) {
+        cursor.classList.remove('grow');
+        cursor.classList.add('grow-small');
+      }
+    });
   });
+};
+
+function showHamburgerMenu() {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.navigation');
+  const navigationLinks = document.querySelectorAll('.navigation__link');
+
+  const handleClick = function () {
+    hamburger.classList.toggle('hamburger--active');
+    hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
+    nav.classList.toggle('navigation--active');
+  }
+
+  for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener('click', function () {
+      hamburger.classList.remove('hamburger--active');
+      nav.classList.remove('navigation--active');
+    })
+  }
+
+  hamburger.addEventListener('click', handleClick);
+};
+
+function contentAnimation() {
+  const tl = gsap.timeline();
+
+  tl.to('.animate', {
+    duration: 1,
+    x: 0,
+    y: 0,
+    scale: 1,
+    stagger: 0.2,
+    delay: 0.1
+  })
+
+  useCustomCursor();
 }
 
-const init = function () {
-  stopAnimations();
-  changeSlide();
+const init = function() {
+  useCustomCursor();
+  showHamburgerMenu();
+  contentAnimation();
 };
 
 document.addEventListener('DOMContentLoaded', init);
